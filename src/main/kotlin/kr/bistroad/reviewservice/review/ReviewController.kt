1fdb17deeb938da2720839c46fe90953fa9b70fe
elsiff <elsiff1002@gmail.com>
2020-08-22 16:30:50 +0900

    Add swagger configuration

--- src/main/kotlin/kr/bistroad/reviewservice/review/ReviewController.kt
+++ src/main/kotlin/kr/bistroad/reviewservice/review/ReviewController.kt
@@ -1,5 +1,7 @@
 package kr.bistroad.reviewservice.review
 
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
 import kr.bistroad.reviewservice.exception.ReviewNotFoundException
 import org.springframework.data.domain.Pageable
 import org.springframework.http.HttpStatus
@@ -8,16 +10,19 @@ import org.springframework.web.bind.annotation.*
 import java.util.*
 
 @RestController
+@Api(tags = ["/stores/**/items/**/reviews"])
 class ReviewController(
     private val reviewService: ReviewService
 ) {
     @GetMapping("/stores/{storeId}/items/{itemId}/reviews/{reviewId}")
+    @ApiOperation("\${swagger.doc.operation.review.get-review.description}")
     fun getReview(
         pathIds: PathIds
     ) = reviewService.readReview(pathIds.storeId!!, pathIds.itemId!!, pathIds.reviewId!!)
         ?: throw ReviewNotFoundException()
 
     @GetMapping("/stores/{storeId}/items/{itemId}/reviews")
+    @ApiOperation("\${swagger.doc.operation.review.get-reviews.description}")
     fun getReviews(
         @PathVariable storeId: UUID,
         @PathVariable itemId: UUID,
@@ -26,6 +31,7 @@ class ReviewController(
     ) = reviewService.searchReviews(storeId, itemId, dto, pageable)
 
     @PostMapping("/stores/{storeId}/items/{itemId}/reviews")
+    @ApiOperation("\${swagger.doc.operation.review.post-review.description}")
     @ResponseStatus(HttpStatus.CREATED)
     fun postReview(
         @PathVariable storeId: UUID,
@@ -34,12 +40,14 @@ class ReviewController(
     ) = reviewService.createReview(storeId, itemId, dto)
 
     @PatchMapping("/stores/{storeId}/items/{itemId}/reviews/{reviewId}")
+    @ApiOperation("\${swagger.doc.operation.review.patch-review.description}")
     fun patchReview(
         pathIds: PathIds,
         @RequestBody dto: ReviewDto.PatchReq
     ) = reviewService.patchReview(pathIds.storeId!!, pathIds.itemId!!, pathIds.reviewId!!, dto)
 
     @DeleteMapping("/stores/{storeId}/items/{itemId}/reviews/{reviewId}")
+    @ApiOperation("\${swagger.doc.operation.review.delete-review.description}")
     fun deleteReview(
         pathIds: PathIds
     ): ResponseEntity<Void> =
